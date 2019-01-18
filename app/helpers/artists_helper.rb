d697077e3e4a60a1f253f53901a70c50db958e8d
Christopher Tompkins <chtompkins026@gmail.com>
2019-01-18 16:52:14 +0000

    Automatically backed up by Learn

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -7,7 +7,7 @@ module ArtistsHelper
    if song.artist.nil?
       select_tag "songpost[artist_id]", options_from_collection_for_select(Artist.all, :id, :name)
     else
-      hidden_field_tag "post[author_id]", post.author_id
+        artist.name
     end
   end 
   
