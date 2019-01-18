a6fe111348c806c84cb182696e25d2a0f8007088
Christopher Tompkins <chtompkins026@gmail.com>
2019-01-18 17:03:41 +0000

    Automatically backed up by Learn

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -5,7 +5,7 @@ module ArtistsHelper
   
   def artist_select(artist, song)
    if song.artist.nil?
-      select_tag "songpost[artist_id]", options_from_collection_for_select(Artist.all, :id, :name)
+      select_tag "song[artist_id]", options_from_collection_for_select(Artist.all, :id, :name)
     else
       artist.name
     end
