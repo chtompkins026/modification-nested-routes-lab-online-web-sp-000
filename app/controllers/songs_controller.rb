da5ac1c1dcb9597dd19be3fae96be49204a392fa
Christopher Tompkins <chtompkins026@gmail.com>
2019-01-18 16:54:35 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -43,8 +43,18 @@ end
   end
 
   def edit
-    @song = Song.find(params[:id])
+  if params[:artist_id]
+    author = Author.find_by(id: params[:author_id])
+    if author.nil?
+      redirect_to authors_path, alert: "Author not found."
+    else
+      @post = author.posts.find_by(id: params[:id])
+      redirect_to author_posts_path(author), alert: "Post not found." if @post.nil?
+    end
+  else
+    @post = Post.find(params[:id])
   end
+end
 
   def update
     @song = Song.find(params[:id])
